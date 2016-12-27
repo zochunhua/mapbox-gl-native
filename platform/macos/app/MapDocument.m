b764fa7f3a16a7900f008d005338aec68faac2f8
Minh Nguyễn <mxn@1ec5.org>
2016-12-27 11:58:20 -0800

    [macos] Layer filter editor
    Added a Layer Filter panel to the bottom of the macosapp document window that edits the selected layer’s predicate. Subclassed NSPredicateEditorRowTemplate to allow for arbitrary key paths in a single template and also to represent an aggregate value as a token field.

--- platform/macos/app/MapDocument.m
+++ platform/macos/app/MapDocument.m
@@ -54,7 +54,8 @@ NS_ARRAY_OF(id <MGLAnnotation>) *MBXFlattenedShapes(NS_ARRAY_OF(id <MGLAnnotatio
 @property (weak) IBOutlet NSArrayController *styleLayersArrayController;
 @property (weak) IBOutlet NSTableView *styleLayersTableView;
 @property (weak) IBOutlet NSMenu *mapViewContextMenu;
-@property (weak) IBOutlet NSSplitView *splitView;
+@property (weak) IBOutlet NSSplitView *styleLayersSplitView;
+@property (weak) IBOutlet NSSplitView *predicateSplitView;
 
 @end
 
@@ -104,9 +105,10 @@ NS_ARRAY_OF(id <MGLAnnotation>) *MBXFlattenedShapes(NS_ARRAY_OF(id <MGLAnnotatio
 
     NSPressGestureRecognizer *pressGestureRecognizer = [[NSPressGestureRecognizer alloc] initWithTarget:self action:@selector(handlePressGesture:)];
     [self.mapView addGestureRecognizer:pressGestureRecognizer];
-
-    [self.splitView setPosition:0 ofDividerAtIndex:0];
-
+    
+    [self.styleLayersSplitView setPosition:0 ofDividerAtIndex:0];
+    [self.predicateSplitView setPosition:DBL_MAX ofDividerAtIndex:0];
+    
     [self applyPendingState];
 }
 
@@ -242,10 +244,10 @@ NS_ARRAY_OF(id <MGLAnnotation>) *MBXFlattenedShapes(NS_ARRAY_OF(id <MGLAnnotatio
  Show or hide the Layers sidebar.
  */
 - (IBAction)toggleLayers:(id)sender {
-    BOOL isShown = ![self.splitView isSubviewCollapsed:self.splitView.arrangedSubviews.firstObject];
+    BOOL isShown = ![self.styleLayersSplitView isSubviewCollapsed:self.styleLayersSplitView.arrangedSubviews.firstObject];
     [NSAnimationContext runAnimationGroup:^(NSAnimationContext * _Nonnull context) {
         context.allowsImplicitAnimation = YES;
-        [self.splitView setPosition:isShown ? 0 : 100 ofDividerAtIndex:0];
+        [self.styleLayersSplitView setPosition:isShown ? 0 : 100 ofDividerAtIndex:0];
         [self.window.toolbar validateVisibleItems];
     } completionHandler:nil];
 }
@@ -336,6 +338,17 @@ NS_ARRAY_OF(id <MGLAnnotation>) *MBXFlattenedShapes(NS_ARRAY_OF(id <MGLAnnotatio
     }
 
     [self.styleLayersArrayController removeObjectsAtArrangedObjectIndexes:indices];
+}
+
+/**
+ Show or hide the Layer Filter pane.
+ */
+- (IBAction)toggleLayerFilter:(id)sender {
+    BOOL isShown = ![self.predicateSplitView isSubviewCollapsed:self.predicateSplitView.arrangedSubviews.lastObject];
+    [NSAnimationContext runAnimationGroup:^(NSAnimationContext * _Nonnull context) {
+        context.allowsImplicitAnimation = YES;
+        [self.predicateSplitView setPosition:isShown ? DBL_MAX : self.mapView.frame.size.height - 100 ofDividerAtIndex:0];
+    } completionHandler:nil];
 }
 
 - (IBAction)setLabelLanguage:(NSMenuItem *)sender {
@@ -882,7 +895,7 @@ NS_ARRAY_OF(id <MGLAnnotation>) *MBXFlattenedShapes(NS_ARRAY_OF(id <MGLAnnotatio
         return YES;
     }
     if (menuItem.action == @selector(toggleLayers:)) {
-        BOOL isShown = ![self.splitView isSubviewCollapsed:self.splitView.arrangedSubviews.firstObject];
+        BOOL isShown = ![self.styleLayersSplitView isSubviewCollapsed:self.styleLayersSplitView.arrangedSubviews.firstObject];
         menuItem.title = isShown ? @"Hide Layers" : @"Show Layers";
         return YES;
     }
@@ -901,6 +914,11 @@ NS_ARRAY_OF(id <MGLAnnotation>) *MBXFlattenedShapes(NS_ARRAY_OF(id <MGLAnnotatio
     }
     if (menuItem.action == @selector(deleteStyleLayers:)) {
         return self.styleLayersTableView.clickedRow >= 0 || self.styleLayersTableView.selectedRow >= 0;
+    }
+    if (menuItem.action == @selector(toggleLayerFilter:)) {
+        BOOL isShown = ![self.predicateSplitView isSubviewCollapsed:self.predicateSplitView.arrangedSubviews.lastObject];
+        menuItem.title = isShown ? @"Hide Layer Filter" : @"Show Layer Filter";
+        return YES;
     }
     if (menuItem.action == @selector(setLabelLanguage:)) {
         menuItem.state = menuItem.tag == _isLocalizingLabels ? NSOnState: NSOffState;
@@ -1057,7 +1075,7 @@ NS_ARRAY_OF(id <MGLAnnotation>) *MBXFlattenedShapes(NS_ARRAY_OF(id <MGLAnnotatio
         }
     }
     if (action == @selector(toggleLayers:)) {
-        BOOL isShown = ![self.splitView isSubviewCollapsed:self.splitView.arrangedSubviews.firstObject];
+        BOOL isShown = ![self.styleLayersSplitView isSubviewCollapsed:self.styleLayersSplitView.arrangedSubviews.firstObject];
         [(NSButton *)toolbarItem.view setState:isShown ? NSOnState : NSOffState];
     }
     return NO;
