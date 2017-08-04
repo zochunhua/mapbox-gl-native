f76eace2c88dddd263b7581772135ea72e938198
Bruno de Oliveira Abinader <bruno@mapbox.com>
2017-08-04 11:05:14 +0300

    [test] One map per pixel ratio in render.test.js --recycle-map

--- platform/node/test/render.test.js
+++ platform/node/test/render.test.js
@@ -5,9 +5,15 @@ const suiteImplementation = require('./suite_implementation');
 const ignores = require('./ignores.json');
 
 let tests;
+let recycleMap = false;
 
 if (process.argv[1] === __filename && process.argv.length > 2) {
-    tests = process.argv.slice(2);
+    if (process.argv[2] === '--recycle-map') {
+        recycleMap = true;
+        tests = process.argv.slice(3);
+    } else {
+        tests = process.argv.slice(2);
+    }
 }
 
-suite.run('native', {tests: tests, ignores: ignores}, suiteImplementation);
+suite.run('native', {tests, ignores, recycleMap}, suiteImplementation);
